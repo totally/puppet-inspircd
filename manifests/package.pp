257331b4d67c027097052dbc9846627b56eda013
Ryan Leary <rleary@mainstreetconnect.us>
2013-11-05 22:08:06 +0000

    manifests/package.pp - option to install from src

--- manifests/package.pp
+++ manifests/package.pp
@@ -17,6 +17,8 @@
 class inspircd::package (
   $ensure = hiera('ensure', $inspircd::params::ensure),
   $use_backport = hiera('use_backport', $inspircd::params::use_backport),
+  $from_src     = hiera('from_src', $inspircd::params::from_src),
+  $inspircd_v   = hiera('inspircd_v', $inspircd::params::inspircd_v),
 ) inherits inspircd::params {
   if $use_backport {
     file { '/etc/apt/preferences.d/inspircd.pref':
@@ -28,8 +30,60 @@ class inspircd::package (
       ensure  => absent,
     }
   }
-  package { 'inspircd':
-    ensure  => $ensure,
-    require => File['/etc/apt/preferences.d/inspircd.pref'],
+
+  if $from_src {
+    # build_essential needed to compile source
+    include build_essential
+    include inspircd::user
+  
+    # irc user is required for make install
+    # Required by ./configure for ascertaining whether GNUTLS is installed
+    package {'pkg-config':
+      ensure  => installed,
+      require => Class['build_essential'],
+    } ->
+    exec {"get inspircd tarball":
+      command     => "wget -O inspircdv${inspircd_v}.tar.gz https://github.com/inspircd/inspircd/archive/v${inspircd_v}.tar.gz",
+      unless      => "test -f inspircdv${inspircd_v}.tar.gz",
+      cwd         => "/root",
+      logoutput   => true,
+    } -> 
+    exec {"unpack inspircd tarball":
+      command     => "tar xfvz inspircdv${inspircd_v}.tar.gz",
+      unless      => "test -d /root/inspircd-${inspircd_v}",
+      cwd         => "/root",
+      logoutput   => true,
+    } -> 
+    exec {"configure inspircd":
+      command     => "perl configure --enable-gnutls --disable-interactive --prefix=/opt/inspircd --uid 39",
+      cwd         => "/root/inspircd-${inspircd_v}/",
+      unless      => "test -f GNUmakefile",
+      logoutput   => true,
+      require     => Class['inspircd::user'],
+    } -> 
+    exec {"make inspircd":
+      command     => "make",
+      cwd         => "/root/inspircd-${inspircd_v}/",
+      unless      => "test -f build/bin/inspircd",
+      logoutput   => true,
+      # This can take a LONG time
+      timeout     => 600,
+    } ->  
+    exec {"make install inspircd":
+      command     => "make install",
+      cwd         => "/root/inspircd-${inspircd_v}/",
+      unless      => "test -d /opt/inspircd",
+      logoutput   => true,
+    } -> 
+    # link to startup script, drops to correct user automatically
+    file {'/etc/init.d/inspircd':
+      ensure  => link,
+      target  => '/opt/inspircd/inspircd',
+    }
+  } else {
+    package { 'inspircd':
+      ensure  => $ensure,
+      require => File['/etc/apt/preferences.d/inspircd.pref'],
+    }
   }
 }
